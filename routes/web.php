9dd06744feb228a75a889c1a90722d8a37607682
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-29 01:39:15 +0200

    Aggiunte route categorie e taf

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ Route::middleware('auth')
     ->group(function () {
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('/posts', 'PostController');
+        Route::resource('/categories', 'CategoryController');
+        Route::resource('/tags', 'TagController');
     });
