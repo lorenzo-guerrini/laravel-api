8c63c06e8d0ce1fff045aef9913966172bb1d521
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-29 02:25:40 +0200

    Separati css e js front e admin

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blog</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{ 'css/front.css' }}">
+</head>
+
+<body>
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ route('admin.home') }}">ADMIN</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+
+    <h1>Hello world!</h1>
+</body>
+
+</html>
